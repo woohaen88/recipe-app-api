2e34d1b8af679833fdd8879d07a35f1df530b5f4
whkim88 <whkim88@lotte.net>
2023-01-27 16:33:47 +0900

    Configured docker compose and checks to wair for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,13 +2,15 @@
 Test custom Django management commands.
 """
 from unittest.mock import patch
-from psycopg2 import OperationalError as Psycopg2Error
+
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-@patch("core.management.commands.wait_for_db.Command.check")
+
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
@@ -16,15 +18,17 @@ class CommandTests(SimpleTestCase):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
 
-        call_command("wait_for_db")
+        call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(databases=["default"])
+        patched_check.assert_called_once_with(databases=['default'])
 
-    @patch("time.sleep")
+    @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test wating for database when getting OperationError"""
-        patched_check.side_effect = [Psycopg2Error]*2 + [OperationalError]*3 + [True]
+        """Test waiting for database when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+            [OperationalError] * 3 + [True]
 
-        call_command("wait_for_db")
+        call_command('wait_for_db')
+
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_once_with(databases=["default"])
+        patched_check.assert_called_with(databases=['default'])
