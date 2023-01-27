670bed4fd96693c624465ea33633f8083f829a9f
whkim88 <whkim88@lotte.net>
2023-01-27 15:30:48 +0900

    Configured docker compose and checks to wair for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(database=["default"])
+                self.check(databases=["default"])
                 db_up = True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write("Database unavailable, wait 1 second...")
