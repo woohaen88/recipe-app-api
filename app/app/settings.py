56fa17aac3110675bbec0923e58ae5d1d248005a
woohaen88 <woohaen88@gmail.com>
2023-01-30 02:27:00 +0900

    create user api

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # app
     "core.apps.CoreConfig",
+    "user.apps.UserConfig",
+    #3rd app
+    "rest_framework",
+    "drf_spectacular",
+
 ]
 
 MIDDLEWARE = [
@@ -135,3 +140,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
