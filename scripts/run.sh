26676c123f2dcc9da0d320a37aae364c936e3fa0
woohaen88 <woohaen88@gmail.com>
2023-01-31 20:35:39 +0900

    deploy

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,6 @@ set -e
 
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
-python manage.py makemigrations
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 1 --master --enable-threads --module app.wsgi
